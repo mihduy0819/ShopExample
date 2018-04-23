7054aa46fcac0c9b0df9cc54228d527956e5db6e
NguyenMinhDuy <minhduy0819@gmail.com>
2018-04-23 20:12:53 +0700

    the first commit

---
+++ ShopExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopExample.MvcApplication" Language="C#" %>
---
+++ ShopExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopExample.Startup))]
+namespace ShopExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
