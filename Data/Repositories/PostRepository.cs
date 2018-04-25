6c4bf389dcc92154130df1979d519e5d3fe90546
NguyenMinhDuy <minhduy0819@gmail.com>
2018-04-25 21:01:41 +0700

    change data project

--- Data/Repositories/PostRepository.cs
+++ Data/Repositories/PostRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Infrastructure;
+using Model.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Data.Repositories
 {
-    class PostRepository
+    public interface IPostRepository : IRepository<Post>
     {
+    }
+
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
     }
 }
