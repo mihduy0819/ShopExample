f5d0730f36c517adcafbe625b06a57f8c84f3b48
NguyenMinhDuy <minhduy0819@gmail.com>
2018-05-12 19:07:27 +0700

    end review angular

---
+++ WebSite/Scripts/spa/app.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+
+myApp.controller('schoolController', schoolController);
+myApp.service('Validator', Validator);
+
+schoolController.$inject = ['$scope', 'Validator'];
+
+function schoolController($scope, Validator) {
+   
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+          return "this is chan";
+        }
+        else {
+            return "this is le";
+        }
+    }
+}
