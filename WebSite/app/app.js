ab841a6fc356cc298dff1b2697591634c527fbb3
NguyenMinhDuy <minhduy0819@gmail.com>
2018-05-14 10:28:42 +0700

    router implement app

---
+++ WebSite/app/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="C:\Users\Dell\Desktop\API_Angular\Git\WebSite\NewFolder1/angular/angular.js" />
+
+(function () {
+    angular.module('shopExample', ['shopExample.products', 'common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ WebSite/app/components/products/products.module.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="C:\Users\Dell\Desktop\API_Angular\Git\WebSite\Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('shopExample.products', ['common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
